20ac67a9c724ede2d5953d8b47cde0eb1c8b66c6
samettopcu88 <samet.topcu.88@icloud.com>
2024-12-30 04:01:58 +0300

    Abstract sınıflar ve migration.

---
+++ CSharpEgitimKampi301.DataAccessLayer/Migrations/202412300026030_initial.cs
@@ -0,0 +1,18 @@
+﻿namespace CSharpEgitimKampi301.DataAccessLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class initial : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Customers", "CustomerStatus", c => c.Boolean(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Customers", "CustomerStatus");
+        }
+    }
+}
